cc7f631cc3493e82d70b7edd0a095f6b8a088daa
BVNDGIADINH\pnqt <phongit_gvnvgd@mail.com>
2026-01-21 09:46:43 +0700

    - thay doi ip

--- src/tools/sync_app/SyncFolders.ps1
+++ src/tools/sync_app/SyncFolders.ps1
@@ -1,8 +1,10 @@
 # --- CONFIGURATION ---
-$ServerList  = Get-Content "C:\Users\hoduc\PycharmProjects\BVGD_Project\src\tools\sync_app\IpAddress.csv"
-$LocalFolder = "C:\Users\hoduc\PycharmProjects\BVGD_Project\dist"
-$RemoteShare = "Users\Public\BackupFolder"
-$LogFolder   = "C:\Users\hoduc\PycharmProjects\BVGD_Project\src\tools\sync_app\logs"
+$ServerList  = Get-Content "C:\Users\pnqt\PycharmProjects\bvgd_project\src\tools\sync_app\IpKhoaKham.csv"
+$LocalFolder = "C:\Users\pnqt\PycharmProjects\bvgd_project\dist"
+$RemoteShare = "ehosbk\EhosBackup"
+$LogFolder   = "C:\Users\pnqt\PycharmProjects\bvgd_project\src\tools\sync_app"
+$User   = "tuanpnq"
+$Password   = "123"
 
 # 1. Ensure log directory exists
 if (!(Test-Path $LogFolder)) {
@@ -25,13 +27,13 @@ foreach ($IP in $ServerList) {
 
         # 3. Handle Password Protection (Authentication)
         # This maps the drive temporarily to allow Robocopy access
-        # $NetPath = "\\$IP\IPC$" # IPC$ is a hidden share used for authentication
-        # net use $NetPath /user:$User $Password /persistent:no | Out-Null
+        $NetPath = "\\$IP\IPC$" # IPC$ is a hidden share used for authentication
+        net use $NetPath /user:$User $Password /persistent:no | Out-Null
 
         Write-Host ">>> Starting Sync to $Destination..." -ForegroundColor Yellow
 
         # 4. Run Robocopy
-        robocopy $LocalFolder $Destination /MIR /LOG:$LogPath
+        robocopy $LocalFolder $Destination /MIR /LOG:$LogPath /TEE
 
         if ($LASTEXITCODE -le 8) {
             Write-Host "[SUCCESS] Synced $IP (Code: $LASTEXITCODE)" -ForegroundColor Green
@@ -40,7 +42,7 @@ foreach ($IP in $ServerList) {
         }
 
         # 5. Cleanup Authentication (Disconnect)
-        # net use $NetPath /delete /y | Out-Null
+        net use $NetPath /delete /y | Out-Null
 
     } else {
         Write-Host "[SKIP] $IP is Offline. Skipping sync." -ForegroundColor Gray
